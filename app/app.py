b4516a2fb6788485cc8f2378a603e53f7216eba7
Prof. B Thangaraju <35483639+BThangaraju@users.noreply.github.com>
2025-02-09 14:46:58 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,5 @@
-
+from flask import Flask, request, jsonify
+app = Flask(name)
+@app.route('/add', methods=['GET']) def add(): a = int(request.args.get('a', 0)) b = int(request.args.get('b', 0)) return jsonify({"result": a + b})
+@app.route('/subtract', methods=['GET']) def subtract(): a = int(request.args.get('a', 0)) b = int(request.args.get('b', 0)) return jsonify({"result": a - b})
+if name == 'main': app.run(host='0.0.0.0', port=5000)
